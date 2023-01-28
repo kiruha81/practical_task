9ae95d4b0744aef02de4f352ec97076592c2a5fc
EC2 Default User <ec2-user@ip-172-31-16-211.ap-northeast-1.compute.internal>
2023-01-28 19:53:34 +0900

    AddFollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :bads, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+
+has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+has_many :followings, through: :relationships, source: :followed
+
+has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+has_many :followers, through: :passive_relationships, source: :follower
+
   has_one_attached :profile_image
 
     validates :name, presence: true
@@ -18,5 +25,16 @@ class User < ApplicationRecord
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
   end
 end
